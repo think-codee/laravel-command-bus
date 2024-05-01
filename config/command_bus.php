4ade401fb0f06e9281edd18d43dbb80b6cc98af1
Razvan Andrei Cureteu <cureteurazvanandrei@gmail.com>
2024-05-01 09:40:12 +0300

    config file

---
+++ config/command_bus.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'buses' => [
+        'command' => [
+            'class' => null,
+            'interface' => null,
+            'alias' => 'bus.command',
+            'middleware' => [],
+            'handler_resolver' => null,
+            'handler_method_resolver' => null,
+        ],
+    ]
+];
---
+++ src/CommandBusServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThinkCodee\Laravel\CommandBus;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandBusServiceProvider extends ServiceProvider
+{
+}
