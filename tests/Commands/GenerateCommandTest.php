5c98bdc16c024bff44917180e4bf9f6c2ffd8cc3
Razvan Andrei Cureteu <cureteurazvanandrei@gmail.com>
2024-05-11 10:26:22 +0300

    finished generate command test

--- tests/Commands/GenerateCommandTest.php
+++ tests/Commands/GenerateCommandTest.php
@@ -9,17 +9,9 @@ use ThinkCodee\Laravel\CommandBus\Tests\TestCase;
 
 class GenerateCommandTest extends TestCase
 {
-    private const STUB_PATH = __DIR__ . '/../../stubs';
-
     protected function tearDown(): void
     {
-        File::shouldReceive('delete')
-            ->andReturnTrue();
-
-        File::delete([
-            app_path('Commands/TestCommand.php'),
-            app_path('Commands/TestCommandHandler.php'),
-        ]);
+        File::deleteDirectory(app_path('Commands'));
 
         parent::tearDown();
     }
@@ -28,53 +20,23 @@ class GenerateCommandTest extends TestCase
     {
         $commandName = 'Test';
 
-        $this->mockCommandAndHandlerGeneration();
-
         $this->artisan('command-bus:make:command', ['name' => $commandName])
-            ->assertSuccessful()
-            ->run();
+            ->assertSuccessful();
 
-        $this->assertFileExists(app_path("Commands/Test/$commandName.php"));
-        $this->assertFileExists(app_path("Commands/Test/{$commandName}Handler.php"));
+        $this->assertFileExists(app_path("Commands/$commandName/$commandName.php"));
+        $this->assertFileExists(app_path("Commands/$commandName/{$commandName}Handler.php"));
     }
 
     public function testItFailsWhenCommandExists(): void
     {
         $commandName = 'Test';
 
-        File::shouldReceive('exists')
-            ->with(app_path("Commands/$commandName"))
-            ->andReturnTrue();
+        $this->artisan('command-bus:make:command', ['name' => $commandName])
+            ->assertSuccessful()
+            ->run();
 
         $this->artisan('command-bus:make:command', ['name' => $commandName])
             ->assertFailed()
             ->run();
-
-        File::shouldReceive('exists')
-            ->andReturnFalse();
-
-        $this->assertFileDoesNotExist(app_path("Commands/Test/$commandName.php"));
-        $this->assertFileDoesNotExist(app_path("Commands/Test/{$commandName}Handler.php"));
-    }
-
-    private function mockCommandAndHandlerGeneration(): void
-    {
-        File::shouldReceive('exists')
-            ->andReturn(false);
-
-        File::shouldReceive('makeDirectory')
-            ->andReturn(true);
-
-        File::shouldReceive('get')
-            ->once()
-            ->andReturn(self::STUB_PATH.'/command.stub');
-
-        File::shouldReceive('get')
-            ->once()
-            ->andReturn(self::STUB_PATH.'/handler.stub');
-
-        File::shouldReceive('put')
-            ->twice()
-            ->andReturn(true);
     }
 }
