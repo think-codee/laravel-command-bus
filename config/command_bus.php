5b5ef9604a868a59fb1ef73b3f0cbf05bd3c9edd
Razvan Andrei Cureteu <cureteurazvanandrei@gmail.com>
2024-05-14 18:35:40 +0300

    default bus

--- config/command_bus.php
+++ config/command_bus.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 return [
     'buses' => [
-//        'default' => [
-//            'class' => null,
-//            'interface' => null,
-//            'alias' => 'bus.default',
-//            'middleware' => [],
-//            'handler_resolver' => \ThinkCodee\Laravel\CommandBus\Resolvers\SuffixHandlerResolver::class,
-//            'handler_method' => 'handle',
-//        ],
+        'default' => [
+            'class' => \ThinkCodee\Laravel\CommandBus\Default\Bus::class,
+            'interface' => \ThinkCodee\Laravel\CommandBus\Contracts\Bus::class,
+            'alias' => 'bus.default',
+            'middleware' => [],
+            'handler_resolver' => \ThinkCodee\Laravel\CommandBus\Resolvers\SuffixHandlerResolver::class,
+            'handler_method' => 'handle',
+        ],
     ]
 ];
---
+++ src/Default/Bus.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ThinkCodee\Laravel\CommandBus\Default;
+
+use ThinkCodee\Laravel\CommandBus\Bus as BaseBus;
+use ThinkCodee\Laravel\CommandBus\Contracts\Bus as BusContract;
+
+class Bus extends BaseBus implements BusContract
+{
+}
